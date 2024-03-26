d4e76559ba497035a84a47405721a0a9691e9f95
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2024-03-26 22:52:07 +0100

    SessionProvider

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,45 +1,17 @@
-import { useEffect, useState } from 'react';
-import { SessionContext, SessionInfo } from './contexts/session'
-import { EVENTS, getDefaultSession, handleIncomingRedirect } from '@inrupt/solid-client-authn-browser';
-import SigninView from './pages/sign_in';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { SessionProvider } from './contexts/session'
+import { Outlet } from 'react-router-dom';
 import { SignoutButton } from './components/buttons/signout_button';
 import FieldSet from './components/ui/FieldSet';
 
 function App() {
-    const [sessionInfo, setSessionInfo] = useState<SessionInfo | null | undefined>(null);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        handleIncomingRedirect({ restorePreviousSession: true })
-            .then((info) => {
-                if (info && info.isLoggedIn) {
-                    setSessionInfo(info as SessionInfo)
-                } else {
-                    setSessionInfo(undefined)
-                }
-            })
-
-        const session = getDefaultSession()
-        
-        session.events.addListener(EVENTS.SESSION_RESTORED, (url: string) => {
-            const newURL = new URL(url);
-            navigate(newURL.pathname + newURL.search)
-        })
-
-        session.events.addListener(EVENTS.LOGOUT, () => setSessionInfo(undefined))
-    }, [])
-
-    if (!sessionInfo) return <SigninView />
-
     return (
-        <SessionContext.Provider value={sessionInfo}>
+        <SessionProvider>
             <Outlet />
 
             <FieldSet header="Account:">
                 <SignoutButton />
             </FieldSet>
-        </SessionContext.Provider>
+        </SessionProvider>
     )
 }
 
--- src/contexts/session.tsx
+++ src/contexts/session.tsx
@@ -1,7 +1,46 @@
-import { ISessionInfo } from "@inrupt/solid-client-authn-browser";
-import { createContext } from "react";
+import { EVENTS, ISessionInfo, getDefaultSession, handleIncomingRedirect } from "@inrupt/solid-client-authn-browser";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SigninView from "../pages/sign_in";
 
 export type SessionInfo = Required<ISessionInfo>;
 export const SessionContext = createContext<SessionInfo | null | undefined>(
   null,
 );
+
+interface Props {
+  children: ReactNode
+}
+
+export function SessionProvider({ children }: Props) {
+  const [sessionInfo, setSessionInfo] = useState<SessionInfo | null | undefined>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    handleIncomingRedirect({ restorePreviousSession: true })
+      .then((sessionInfo) => {
+        if (sessionInfo && sessionInfo.isLoggedIn) {
+          setSessionInfo(sessionInfo as SessionInfo)
+        } else {
+          setSessionInfo(undefined)
+        }
+      })
+
+    const session = getDefaultSession()
+
+    session.events.addListener(EVENTS.SESSION_RESTORED, (url: string) => {
+      const newURL = new URL(url);
+      navigate(newURL.pathname + newURL.search)
+    })
+
+    session.events.addListener(EVENTS.LOGOUT, () => setSessionInfo(undefined))
+  }, [])
+
+  if (!sessionInfo) return <SigninView />
+
+  return (
+    <SessionContext.Provider value={sessionInfo}>
+      {children}
+    </SessionContext.Provider>
+  )
+}
